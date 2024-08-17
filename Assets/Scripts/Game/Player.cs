9d5bc93ff19ca47bd83ede5840f72c30f98c1501
LWCoding <DiamondMob01@gmail.com>
2024-08-17 02:25:24 -0700

    Added tilemap tag to show behind player; created mostly empty playermovement script

---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("Object Assignments")]
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+}
