62a8754435d1541ee18476e89cf7424642c56e97
LWCoding <DiamondMob01@gmail.com>
2024-08-18 00:17:05 -0700

    Created pan object that is cooked when interacting with stove

--- Assets/Scripts/Game/Tiles/StoveTileObject.cs
+++ Assets/Scripts/Game/Tiles/StoveTileObject.cs
@@ -8,14 +8,31 @@ public class StoveTile : TileObject
 
     [Header("Object Assignments")]
     [SerializeField] private GameObject _stoveUIPrefab;
+    [SerializeField] private GameObject _stovePanPrefab;
 
     private Vector3 _popupOffset = new(0, 1.5f);
 
+    private GameObject _panObject = null;  // If no pan on stove, null, else GameObject
+
     public override void Interact(Vector3Int position)
     {
+        // If we have a pan, interact with the pan instead!
+        if (_panObject != null)
+        {
+            Destroy(_panObject);
+            _panObject = null;
+            return;
+        }
+
+        // Interact with the stove and cook some food
         IsInteractable = false;
-        Vector3 popupLocation = (Vector3)position + new Vector3(0.5f, 0.5f, 0) + _popupOffset;
-        PopupManager.Instance.SpawnPopupThen(_stoveUIPrefab, popupLocation, 8, () =>
+        Vector3 stovePosition = (Vector3)position + new Vector3(0.5f, 0.5f, 0);
+
+        // Create a pan to show at the stove
+        _panObject = Instantiate(_stovePanPrefab, stovePosition, Quaternion.identity);
+
+        // Spawn the popup above the stove
+        PopupManager.Instance.SpawnPopupThen(_stoveUIPrefab, stovePosition + _popupOffset, 8, () =>
         {
             IsInteractable = true;
         });
